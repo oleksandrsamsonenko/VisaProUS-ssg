f0cbc9719aec94554547ed2a9e85290007064995
Oleksandr Samsonenko <invizibji@gmail.com>
2024-04-03 17:01:02 +0300

    add some SEO properties for testing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,8 +21,17 @@ useHead({
       content:
         'Serving your immigration needs, Qualified assistance in preparing documents for successful immigration to the USA. There for our clients whenever they need us'
     },
-    { name: 'robots', content: 'all' },
-    { property: 'og:site_name', content: 'United Legal Alliance' }
+    { name: 'robots', content: 'index, follow' },
+    { name: 'x-robots-tag', content: 'index, follow' },
+    { property: 'og:site_name', content: 'United Legal Alliance by Byteoff' },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:title', content: 'Serving your immigration needs' },
+    { property: 'og:image', content: 'https://visa-pro-us-ssg.vercel.app/OurServices/3.jpg' },
+    { property: 'og:url', content: 'https://visa-pro-us-ssg.vercel.app/' },
+    {
+      property: 'og:description',
+      content: 'Qualified assistance in preparing documents for successful immigration to the USA.'
+    }
   ]
 })
 </script>
