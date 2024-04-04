8a3d3922eb843c0a508a6a13c6a071d70579226b
Oleksandr Samsonenko <invizibji@gmail.com>
2024-04-04 14:35:00 +0300

    add sitemap and robots.txt to build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
+import generateSitemap from 'vite-ssg-sitemap'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  ssgOptions: {
+    onFinished() {
+      generateSitemap()
     }
   }
 })
